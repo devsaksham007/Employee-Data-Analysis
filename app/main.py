db22b02ec8ffca2793ef31a11d8eb60ac9e4359b
devsaksham007 <sakshamkavya1@gmail.com>
2026-09-20 23:54:42 +0530

    Fix template response to correctly pass request object in root endpoint

--- app/main.py
+++ app/main.py
@@ -20,4 +20,4 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html", {})
